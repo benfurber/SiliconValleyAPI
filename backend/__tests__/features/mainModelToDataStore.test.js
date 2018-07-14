55720b32ed428f54913bc1007fc7ebb39f6ee428
benfurber <ben@furber-consultancy.com>
2018-07-14 10:48:25 +0100

    Add API call to route
    * Route in app calls films model which responses with an array

--- backend/__tests__/features/mainModelToDataStore.test.js
+++ backend/__tests__/features/mainModelToDataStore.test.js
@@ -9,4 +9,12 @@ describe('app', () => {
       expect(response.statusCode).toEqual(200)
     })
   })
+
+  describe('/films', () => {
+    it('responds with JSON', async () => {
+      let response = await request(app).get('/films')
+      let data = response.res.text
+      expect(data.length).not.toEqual(0)
+    })
+  })
 })
--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
+const Films = require('./models/films')
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello World')
+})
+
+app.get('/films', async (req, res) => {
+  let films = new Films()
+  let data = await films.readFilms()
+  res.status(200).json(data)
 })
 
 module.exports = app
