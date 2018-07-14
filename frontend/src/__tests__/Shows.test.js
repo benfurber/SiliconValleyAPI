b3cc1fee5542f77b96cb3e42b51344c0c90cefed
benfurber <ben@furber-consultancy.com>
2018-07-14 11:46:47 +0100

    Add Shows component and rollback React changes
    Added a basic component for shows. Also, some changes I made to the
    package were causing some weird test errors so I removed react and
    started again. Will now add packages slowly as I go.

--- frontend/src/__tests__/Shows.test.js
+++ frontend/src/__tests__/Shows.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Shows from '../Shows';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Shows />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
