a594f62331ac6dd1da77f32ff6f0bcea80cc8d42
benfurber <ben@furber-consultancy.com>
2018-07-14 18:23:45 +0100

    Add by season to the API
    * Route /episodes/season/:num added which links to films.readFilms(num)
    * Route for 404 added (mainly to help feature tests be more robust)

--- backend/__tests__/features/mainModelToDataStore.test.js
+++ backend/__tests__/features/mainModelToDataStore.test.js
@@ -10,11 +10,29 @@ describe('app', () => {
     })
   })
 
-  describe('/films', () => {
+  describe('/episodes', () => {
     it('responds with JSON', async () => {
-      let response = await request(app).get('/films')
-      let data = response.res.text
-      expect(data.length).not.toEqual(0)
+      let response = await request(app).get('/episodes')
+      let data = JSON.parse(response.res.text)
+
+      expect(typeof data).toEqual('object')
+    })
+  })
+
+  describe('/episodes/season/', () => {
+    it('responds with JSON for only an individual season', async () => {
+      let response = await request(app).get('/episodes/season/2')
+      let data = JSON.parse(response.res.text)
+
+      expect(typeof data).toEqual('object')
+    })
+  })
+
+  describe('/anything-else', () => {
+    it('responds with a 404', async () => {
+      let response = await request(app).get('/12331e')
+
+      expect(response.statusCode).toEqual(404)
     })
   })
 })
